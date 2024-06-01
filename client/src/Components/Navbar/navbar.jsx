0c220cd3f1023c6c5e55a43f9e22735250085ef7
Sambit Mondal <sambitmondal2005@gmail.com>
2024-06-02 01:09:59 +0530

    Remove Hire talent and admin

--- client/src/Components/Navbar/navbar.jsx
+++ client/src/Components/Navbar/navbar.jsx
@@ -6,27 +6,25 @@ function Navbar() {
   return (
     <div>
       <nav className='navbar-large-screens flex fixed top-0'>
-        <div className='nav-logo flex gap-5 mr-36 font-bold'>
-          <img className='w-10 h-10' src={logo} alt='InternQuest logo'></img>
-          <p className='nav-text'>
-            InternQuest
-          </p>
+        <div className='nav-main flex flex-1'>
+          <div className='nav-logo flex gap-5 mr-36 font-bold'>
+            <img className='w-10 h-10' src={logo} alt='InternQuest logo'></img>
+            <p className='nav-text'>
+              InternQuest
+            </p>
+          </div>
+          <div className='elements flex gap-20 flex-1'>
+            <p>Internships <i class="bi bi-caret-down-fill"></i></p>
+            <p>Jobs <i class="bi bi-caret-down-fill"></i></p>
+          </div>
+          <div className='search flex ml-36 mr-24'>
+            <i class="bi bi-search"></i>
+            <input placeholder='Search' />
+          </div>
         </div>
-        <div className='elements flex gap-10'>
-          <p>Internships <i class="bi bi-caret-down-fill"></i></p>
-          <p>Jobs <i class="bi bi-caret-down-fill"></i></p>
-        </div>
-        <div className='search flex'>
-          <i class="bi bi-search"></i>
-          <input placeholder='Search' />
-        </div>
-        <div className='user-auth flex gap-5 mr-10'>
+        <div className='user-auth flex gap-5'>
           <button className='login'>Login</button>
           <button className='register'>Register</button>
-        </div>
-        <div className='admin-auth flex gap-5'>
-          <button className='hire-talent'>Hire Talent</button>
-          <button className='admin'>Admin</button>
         </div>
       </nav>
     </div>
