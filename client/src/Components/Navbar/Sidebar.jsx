608e52e31c054fd05ef1d648dd5ad9e11d90c468
Sambit Mondal <sambitmondal2005@gmail.com>
2024-06-09 23:27:49 +0530

    Update Sidebar.jsx

--- client/src/Components/Navbar/Sidebar.jsx
+++ client/src/Components/Navbar/Sidebar.jsx
@@ -1,8 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 function Sidebar() {
-  return (
-    <div>Sidebar</div>
+
+    // Sidebar 
+    const [sidebarOpen, setSidebarOpen] = useState(false);
+
+    // Sidebar open
+    const openSidebar = () => {
+        setSidebarOpen(true)
+    }
+
+    // Sidebar close
+    const closeSidebar = () => {
+        setSidebarOpen(false)
+    }
+
+    useEffect(() => {
+        const handleOutsideClick = (e) => {
+            if(sidebarOpen && !e.target.closest('.sidebar') && !e.target.closest('open-btn')) {
+                closeSidebar();
+            }
+        }
+
+        document.addEventListener('click', handleOutsideClick);
+
+        return () => {
+            document.removeEventListener('click', handleOutsideClick);
+        }
+    }, [sidebarOpen]);
+
+  return [sidebarOpen] (
+    <div>
+        
+    </div>
   )
 }
 
