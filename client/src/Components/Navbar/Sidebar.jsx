ebdf8bab50c334c4b083defc160284370f49eeb7
Sambit Mondal <sambitmondal2005@gmail.com>
2024-06-11 23:52:33 +0530

    Created Sidebar (for mobiles)

--- client/src/Components/Navbar/Sidebar.jsx
+++ client/src/Components/Navbar/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import logo from '../../Assets/logo.png'
 import './sidebar.css'
 
@@ -7,39 +7,62 @@ function Sidebar() {
     // Initializing state variable
     const [sidebarOpen, setSidebarOpen] = useState(false);
 
+    const ref = useRef();
+
+    useEffect(() => {
+    const handleOutsideClick = (e) => {
+        if (
+            sidebarOpen && ref.current && !ref.current.contains(e.target)
+        ) {
+            setSidebarOpen(false);
+        }
+    };
+
+    // Closes the sidebar when a click outside the sidebar is detected
+    document.addEventListener('mousedown', handleOutsideClick);
+
+    return () => {
+
+        //Cleanup the event listener
+        document.removeEventListener('mousedown', handleOutsideClick);
+    };
+}, [sidebarOpen]);
+
     return (
         <div>
-            <nav className='navbar-small-screens flex flex-1 items-center content-center pt-4 pb-4 w-full'>
-                <div>
-                    <button className='toggle border-0 pr-3' onClick={() => setSidebarOpen((prev) => !prev)}>
-                        {sidebarOpen ? (
-                            <i class='bi bi-x-lg text-2xl'></i>
-                        ) : (
-                            <i class="bi bi-list text-2xl"></i>
-                        )}
-                    </button>
-                </div>
-                    <div className={`sidemenu${sidebarOpen? '  show-menu' : ''}`}>
-                        <p>Internships</p>
-                        <p>Jobs</p>
-                        <p>Courses</p>
-                        <p>Placement Guarantee Courses</p>
-                        <p>Study Abroad</p>
-                        <p>Study in India</p>
-                        <p>Online Degrees</p>
-                        <p>Contact Us</p>
-                        <p>Get our App</p>
-                        <hr></hr>
-                        <p>Register as Student</p>
-                        <p>Register as Employer</p>
-                        <p>Login</p>
-                    </div>
-                <div className='nav-main flex gap-28'>
-                    <div className='nav-logo-small'>
-                        <img className='w-11 h-11' src={logo} alt='InternQuest logo'></img>
+            <nav ref={ref} className='navbar-small-screens flex flex-wrap items-center content-center pt-4 pb-4 w-full'>
+                <div className='nav-main grid'>
+                    <div className='logo-and-toggle flex'>
+                        <button className='toggle border-0 z-50' onClick={() => setSidebarOpen((prev) => !prev)}>
+                            {sidebarOpen ? (
+                                <i class='bi bi-x-lg text-2xl'></i>
+                            ) : (
+                                <i class="bi bi-list text-2xl"></i>
+                            )}
+                        </button>
+
+                        <div className={`sidemenu${sidebarOpen ? '  show-menu' : ''}`}>
+                            <p>Internships</p>
+                            <p>Jobs</p>
+                            <p className='courses'>Courses <span className='offer-text'>OFFER</span></p>
+                            <p>Placement Guarantee Courses</p>
+                            <p>Study Abroad</p>
+                            <p>Study in India</p>
+                            <p>Online Degrees</p>
+                            <p>Contact Us</p>
+                            <p>Get our App</p>
+                            <hr></hr>
+                            <p>Register - As a Student</p>
+                            <p>Register - As an Employer</p>
+                            <p>Login</p>
+                        </div>
+
+                        <div className='nav-logo-small'>
+                            <img className='w-auto h-12' src={logo} alt='InternQuest logo'></img>
+                        </div>
                     </div>
                     <div className='user-auth flex'>
-                        <button className='register cursor-pointer w-28'>Register</button>
+                        <button className='register cursor-pointer w-auto h-auto items-end content-end'>Register <i class="bi bi-caret-down-fill"></i></button>
                     </div>
                 </div>
             </nav>
