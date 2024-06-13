c02e451b32b29f1794e80f933175432ca27ea7a9
Sambit Mondal <sambitmondal2005@gmail.com>
2024-06-13 21:54:45 +0530

    Update Sidebar.jsx

--- client/src/Components/Navbar/Sidebar.jsx
+++ client/src/Components/Navbar/Sidebar.jsx
@@ -10,23 +10,27 @@ function Sidebar() {
     const ref = useRef();
 
     useEffect(() => {
-    const handleOutsideClick = (e) => {
-        if (
-            sidebarOpen && ref.current && !ref.current.contains(e.target)
-        ) {
-            setSidebarOpen(false);
-        }
-    };
+        const handleOutsideClick = (e) => {
+            if (
+                sidebarOpen && ref.current && !ref.current.contains(e.target)
+            ) {
+                setSidebarOpen(false);
+            }
+        };
 
-    // Closes the sidebar when a click outside the sidebar is detected
-    document.addEventListener('mousedown', handleOutsideClick);
+        // Closes the sidebar when a click outside the sidebar is detected
+        document.addEventListener('mousedown', handleOutsideClick);
 
-    return () => {
+        return () => {
 
-        //Cleanup the event listener
-        document.removeEventListener('mousedown', handleOutsideClick);
-    };
-}, [sidebarOpen]);
+            //Cleanup the event listener
+            document.removeEventListener('mousedown', handleOutsideClick);
+        };
+    }, [sidebarOpen]);
+
+
+    // Defining an user
+    const user = null;
 
     return (
         <div>
@@ -42,6 +46,22 @@ function Sidebar() {
                         </button>
 
                         <div className={`sidemenu${sidebarOpen ? ' show-menu' : ''}`}>
+                            {user ? (
+                                <div className='profile'>
+                                    <div className='sidebar-user-photo'>
+                                        <img src={user.photo} alt='' />
+                                    </div>
+                                    <div className='sidebar-user-details'>
+                                        <div className='sidebar-username'>
+                                            <p>{user.username}</p>
+                                        </div>
+                                        <div className='sidebar-user-email'>
+                                            <p>{user.email}</p>
+                                        </div>
+                                    </div>
+                                    <hr></hr>
+                                </div>
+                            ) : ""}
                             <p>Internships</p>
                             <p>Jobs</p>
                             <p className='courses'>Courses <span className='offer-text'>OFFER</span></p>
@@ -49,12 +69,32 @@ function Sidebar() {
                             <p>Study Abroad</p>
                             <p>Study in India</p>
                             <p>Online Degrees</p>
-                            <p>Contact Us</p>
-                            <p>Get our App</p>
+                            {user ? (
+                                <>
+                                    <p>PepsiCo community impact initiative</p>
+                                </>
+                            ) : (
+                                <>
+                                    <p>Contact Us</p>
+                                    <p>Get our App</p>
+                                </>
+                            )}
                             <hr></hr>
-                            <p>Register - As a Student</p>
-                            <p>Register - As an Employer</p>
-                            <p>Login</p>
+                            {user ? (
+                                <>
+                                    <p>My Applications</p>
+                                    <p>My Bookmarks</p>
+                                    <p>Edit Resume</p>
+                                    <p>Edit Preferences</p>
+                                    <p>More</p>
+                                </>
+                            ) : (
+                                <>
+                                    <p>Register - As a Student</p>
+                                    <p>Register - As an Employer</p>
+                                    <p>Login</p>
+                                </>
+                            )}
                         </div>
 
                         <div className='nav-logo-small'>
